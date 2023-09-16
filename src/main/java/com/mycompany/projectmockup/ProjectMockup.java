21a526a7966198269944a76e9b4109838fbfe383
PABLO MORERA <PABLO MORERA@DESKTOP-OPTDJEA.bbrouter>
2023-09-16 02:50:23 -0600

    Reporte de Tipos de Instrumento

--- src/main/java/com/mycompany/projectmockup/ProjectMockup.java
+++ src/main/java/com/mycompany/projectmockup/ProjectMockup.java
@@ -11,11 +11,14 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfDocument;
 import com.itextpdf.text.pdf.PdfWriter;
 import Presentation.Controller;
+import Presentation.View;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 /**
  *
@@ -41,5 +44,13 @@ public class ProjectMockup {
         document.add(new Paragraph("Fecha: " + f.format(new Date())));
         document.add(new Paragraph(line));
         document.close();
+        
+        View vista = new View();
+        Controller ctrl = new Controller(vista);
+        vista.setTitle("Sistema Laboratorios");
+        vista.setLocationRelativeTo(null);
+        vista.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        vista.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        vista.setVisible(true);
     }
 }
